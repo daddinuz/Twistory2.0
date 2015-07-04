1206b8bcb35ec9837de19bb3d89e80b226563fca
Davide Di Carlo <davide_di_carlo@yahoo.it>
2015-07-04 11:42:36 +0200

    Trigger Error Email

--- app/mailers/mailer.rb
+++ app/mailers/mailer.rb
@@ -13,12 +13,12 @@ class Mailer < ActionMailer::Base
 	end
 
 	def trigger_error_email(error_message)
-		@error_message = error_message
+		@error_message = error_message.to_s
 		
 		if Rails.env.production?
-			mail(:to => 'info@ragazzidel99.it', :subject => error_message.to_s)
+			mail(:to => 'info@ragazzidel99.it', :subject => @error_message)
 		else
-			mail(:to => 'twittwar95@gmail.com', :subject => error_message.to_s)
+			mail(:to => 'twittwar95@gmail.com', :subject => @error_message)
 		end
 	end
 end
